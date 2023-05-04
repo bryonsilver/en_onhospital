98f68e65d30f744314cb79e9984e90bb679518f9
siucrystal <76192739+bryonsilver@users.noreply.github.com>
2023-05-04 15:39:27 +0900

    비급여 수정 / 진료안내 수정

--- onhospital_en/js/unpaid.js
+++ onhospital_en/js/unpaid.js
@@ -4,30 +4,6 @@ $(document).ready(function(){
 	const selec = urlParams.get('selec');
 	console.log('selec',selec)
 
-    $(window).resize(function(){
-        // var alert_ck= false;
-		var width = window.innerWidth;
-		if(width < 1150){
-			$('.danger').css({opacity:'1'})
-            alert('모바일에서는 보기 어려움으로 모니터화면으로 봐주시기 바랍니다.')
-            // alert_ck= ture; 
-			// ture false 하면 햄버거 MENU까지 안먹히게 됨... 방법 모색....
-		}
-	}).resize();
-
-
-	// if($('.carousel_txt').length) {
-   //     console.log(1)
-              
-   //     $(window).on('scroll', function() {
-   //         if ($(window).scrollTop() >= 500) {
-   //             $('#quick').attr('style', 'top:130px !important');
-   //         } else {
-    //            $('#quick').attr('style', 'top:837px !important');
-    //        }
-    //    })
-   //  } else {console.log('false')}
-
 
    
    // 구글번역 뒤로갔다가 돌아왔을때 번역기능 사라지는 것 제압
